e0905f2e45ff4a83dca8020319e1bb649676c741
Payton Shaltis <shaltisp@gmail.com>
2022-12-29 17:05:28 -0500

    Adds network check for TELLO-xxx drone network

--- drone.py
+++ drone.py
@@ -1,19 +1,19 @@
 from djitellopy import tello
-
+import subprocess
 
 def connect_to_drone() -> tello.Tello:
   """
   Connects to the drone and prints the battery level. Prints an error message if
   the drone cannot be connected to the program. Returns the drone object or .
   """
-
-  drone = tello.Tello()
-  if drone.connect():
+  try:
+    drone = tello.Tello()
+    drone.connect()
     print("Drone connected!")
     print("Battery: " + str(drone.get_battery()))
     return drone
-  else:
-    print("Error connecting to drone!")
+  except:
+    print("Error connecting to drone.")
     exit()
 
 def main() -> None:
@@ -22,8 +22,14 @@ def main() -> None:
   or face detection, drone adjustement, and drone movement.
   """
 
-  print("")
-  drone = connect_to_drone()
+  # Connect to drone only if on the correct network.
+  print("Checking for connection to drone network...")
+  if subprocess.check_output(['netsh', 'WLAN', 'show', 'interfaces']).decode('utf-8').find("TELLO-") != -1:
+    print("Connected to drone network!")
+    drone = connect_to_drone()
+  else:
+    print("Error: not connected to drone network!")
+    exit()
 
 if __name__ == "__main__":
   main()
