71a70a5f624588975411e3e7c2c3394fce4a57ea
Payton Shaltis <shaltisp@gmail.com>
2022-12-29 17:44:30 -0500

    Refactors mediapipe code for clarification.

--- drone.py
+++ drone.py
@@ -2,9 +2,55 @@ from djitellopy import tello
 import mediapipe as mp
 import subprocess, cv2
 
-mp_face_mesh = mp.solutions.face_mesh
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+"""
+Mediapipe face mesh constants.
+"""
+MP_FACE_MESH = mp.solutions.face_mesh
+MP_DRAWING = mp.solutions.drawing_utils
+MP_DRAWING_STYLES = mp.solutions.drawing_styles
+
+def add_mp_face_mesh(face_mesh_params, image):
+  """
+  Modifies the passed image and returns a new one. This image is modified with
+  the mediapipe face mesh overlay for detecting facial features. Also requires
+  the tuple of face mesh params.
+  """
+  # To improve performance, optionally mark the image as not writeable to
+  # pass by reference.
+  image.flags.writeable = False
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  results = face_mesh_params.process(image)
+
+  # Draw the face mesh annotations on the image.
+  image.flags.writeable = True
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  if results.multi_face_landmarks:
+    for face_landmarks in results.multi_face_landmarks:
+      MP_DRAWING.draw_landmarks(
+          image=image,
+          landmark_list=face_landmarks,
+          connections=MP_FACE_MESH.FACEMESH_TESSELATION,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=MP_DRAWING_STYLES
+          .get_default_face_mesh_tesselation_style())
+      MP_DRAWING.draw_landmarks(
+          image=image,
+          landmark_list=face_landmarks,
+          connections=MP_FACE_MESH.FACEMESH_CONTOURS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=MP_DRAWING_STYLES
+          .get_default_face_mesh_contours_style())
+      MP_DRAWING.draw_landmarks(
+          image=image,
+          landmark_list=face_landmarks,
+          connections=MP_FACE_MESH.FACEMESH_IRISES,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=MP_DRAWING_STYLES
+          .get_default_face_mesh_iris_connections_style())
+
+  # Return the modified image.
+  return image
+
 
 def connect_to_drone() -> tello.Tello:
   """
@@ -47,49 +93,17 @@ def main() -> None:
         drone.land()
       case "streamon":
         drone.streamon()
-        with mp_face_mesh.FaceMesh(
+
+        # Add the face mesh and display the live feed.
+        with MP_FACE_MESH.FaceMesh(
           max_num_faces=1,
           refine_landmarks=True,
           min_detection_confidence=0.5,
           min_tracking_confidence=0.5) as face_mesh:
+          
+          # Continue showing the live feed until the user exits.
           while True:
-            image = drone.get_frame_read().frame
-
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = face_mesh.process(image)
-
-            # Draw the face mesh annotations on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            if results.multi_face_landmarks:
-              for face_landmarks in results.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_tesselation_style())
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_contours_style())
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_IRISES,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                    .get_default_face_mesh_iris_connections_style())
-
-            # Display the image.
-            cv2.imshow('MediaPipe Face Mesh', image)
+            cv2.imshow('MediaPipe Face Mesh', add_mp_face_mesh(face_mesh, drone.get_frame_read().frame))
             if cv2.waitKey(5) & 0xFF == 27:
               break
 
